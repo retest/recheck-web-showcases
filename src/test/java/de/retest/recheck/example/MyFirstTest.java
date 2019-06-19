e0726d353acea6d878fa74673b9ddbdcff4ec228
Jeremias Roessler <roessler@retest.de>
2019-06-19 11:02:39 +0200

    Add first test together with HTML page

---
+++ src/test/java/de/retest/recheck/example/MyFirstTest.java
@@ -0,0 +1,39 @@
+package de.retest.recheck.example;
+
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import de.retest.web.selenium.By;
+import de.retest.web.selenium.RecheckDriver;
+
+public class MyFirstTest {
+
+	RecheckDriver driver;
+
+	@Before
+	public void setup() {
+		driver = new RecheckDriver( new ChromeDriver());
+	}
+
+	@Test
+	public void check_formPage() throws Exception {
+		driver.startTest();
+		String url = Paths.get( "src/test/resources/formPage.html" ).toUri().toURL().toString();
+		driver.get(url);
+
+		driver.findElement(By.id("email")).sendKeys("Emil");
+		driver.findElement(By.id("age")).sendKeys("16");
+		driver.findElement(By.name("login")).submit();
+
+		driver.capTest();
+	}
+
+	@After
+	public void tearDown() throws InterruptedException {
+		driver.quit();
+	}
+}
