8334db9412cd6762a7ac5d4edbf1b12ddfd0f270
Jeremias Roessler <roessler@retest.de>
2019-06-19 11:03:11 +0200

    Add Golden Master and recheck files

---
+++ .retest/recheck.ignore.js
@@ -0,0 +1,9 @@
+// Here you can implement ignore rules for recheck in javascript.
+// Please do not delete this file, even if it is empty.
+
+// You can implement either of these two functions:
+// function matches(element) {}
+// function matches(element, diff) {}
+
+// You can find more details and example rules at: 
+// https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
