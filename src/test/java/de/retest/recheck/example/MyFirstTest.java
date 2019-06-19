ad8f7ad9e5ec4f71da24ebb5b7cfffd3c7b74979
Jeremias Roessler <roessler@retest.de>
2019-06-19 11:07:37 +0200

    Adapt test to have it execute on Travis

--- src/test/java/de/retest/recheck/example/MyFirstTest.java
+++ src/test/java/de/retest/recheck/example/MyFirstTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import de.retest.web.selenium.By;
 import de.retest.web.selenium.RecheckDriver;
@@ -16,7 +17,13 @@ public class MyFirstTest {
 
 	@Before
 	public void setup() {
-		driver = new RecheckDriver( new ChromeDriver());
+		final ChromeOptions opts = new ChromeOptions();
+		opts.addArguments(
+				// Enable headless mode to execute on Travis
+				"--headless",
+				// Use Chrome in container-based Travis CI enviroment (see https://docs.travis-ci.com/user/chrome#Sandboxing).
+				"--no-sandbox" );
+		driver = new RecheckDriver( new ChromeDriver(opts));
 	}
 
 	@Test
