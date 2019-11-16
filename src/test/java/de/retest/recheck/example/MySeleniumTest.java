920ffc2f6392e3bf232244b067c3a065fbee92f0
Jeremias Roessler <roessler@retest.de>
2019-11-16 14:19:13 +0300

    Turn test into recheck test

--- src/test/java/de/retest/recheck/example/MySeleniumTest.java
+++ src/test/java/de/retest/recheck/example/MySeleniumTest.java
@@ -1,33 +1,31 @@
 package de.retest.recheck.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import de.retest.recheck.RecheckOptions;
+import de.retest.recheck.persistence.ClassAndMethodBasedShortNamingStrategy;
+import de.retest.recheck.persistence.GradleProjectLayout;
+import de.retest.web.selenium.RecheckDriver;
 
 
 class MySeleniumTest {
 
-	WebDriver driver;
+	RecheckDriver driver;
 
 	@BeforeEach
 	void setup() {
-		final ChromeOptions opts = new ChromeOptions();
-		opts.addArguments(
-				// Enable headless mode for faster execution.
-				"--headless",
-				// Use Chrome in container-based Travis CI enviroment (see https://docs.travis-ci.com/user/chrome#Sandboxing).
-				"--no-sandbox",
-				// Fix window size for stable results.
-				"--window-size=1200,800" );
-		driver = new ChromeDriver( opts );
+		RecheckOptions opts = RecheckOptions.builder(). //
+				projectLayout(new GradleProjectLayout()). //
+				namingStrategy(new ClassAndMethodBasedShortNamingStrategy() ). //
+				enableReportUpload().
+				build();
+		driver = new RecheckDriver( new ChromeDriver(), opts );
 	}
 
 	@Test
@@ -39,7 +37,7 @@ class MySeleniumTest {
 		driver.findElement(By.id("password")).sendKeys("secret");
 		driver.findElement(By.id("sign-in")).click();
 
-		assertEquals(driver.findElement(By.tagName("h4")).getText(), "Success!");
+		driver.capTest();
 	}
 
 	@AfterEach
