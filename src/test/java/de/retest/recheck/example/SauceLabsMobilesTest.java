f6406d83d024e6cb4f6178c57c8d4b0147ab92fe
Martin Vietz <martin.vietz@ubs-hainer.com>
2021-01-25 11:36:16 +0100

    Update remote test examples

--- src/test/java/de/retest/recheck/example/SauceLabsMobilesTest.java
+++ src/test/java/de/retest/recheck/example/SauceLabsMobilesTest.java
@@ -4,6 +4,7 @@ import java.net.URL;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,10 +14,14 @@ import de.retest.recheck.Recheck;
 import de.retest.recheck.RecheckImpl;
 import de.retest.recheck.RecheckProperties;
 
+@Disabled // TODO 1. remove this line me
 class SauceLabsMobilesTest {
 
-	private static final String sauceUserName = "roessler1";
-	private static final String sauceAccessKey = "49342fea-55f6-4c60-9402-aac73de16828";
+	// TODO 2. Enter your username and access key
+	private static final String sauceUserName = "YOUR USERNAME";
+	private static final String sauceAccessKey = "KEEP_ME_SECRET";
+
+	// TODO 3. Run this test with cloud based browser
 
 	private static final String remoteUrl = "http://ondemand.eu-central-1.saucelabs.com/wd/hub";
 	//	private static final String targetUrl = "https://assets.retest.org/demos/app/demo-app.html";
@@ -30,20 +35,20 @@ class SauceLabsMobilesTest {
 	@Test
 	public void checkSamsungGalaxy() throws Exception {
 		re = new RecheckImpl();
-		re.startTest(testName);
-		DesiredCapabilities capabilities = DesiredCapabilities.android();
-		capabilities.setCapability("username", sauceUserName);
-		capabilities.setCapability("accessKey", sauceAccessKey);
-		capabilities.setCapability("deviceName","Samsung Galaxy S8 Plus GoogleAPI Emulator");
-		capabilities.setCapability("deviceOrientation", "portrait");
-		capabilities.setCapability("browserName", "Chrome");
-		capabilities.setCapability("platformVersion", "8.0");
-		capabilities.setCapability("platformName","Android");
-		driver = new RemoteWebDriver(new URL(remoteUrl), capabilities);
+		re.startTest( testName );
+		final DesiredCapabilities capabilities = DesiredCapabilities.android();
+		capabilities.setCapability( "username", sauceUserName );
+		capabilities.setCapability( "accessKey", sauceAccessKey );
+		capabilities.setCapability( "deviceName", "Samsung Galaxy S8 Plus GoogleAPI Emulator" );
+		capabilities.setCapability( "deviceOrientation", "portrait" );
+		capabilities.setCapability( "browserName", "Chrome" );
+		capabilities.setCapability( "platformVersion", "8.0" );
+		capabilities.setCapability( "platformName", "Android" );
+		driver = new RemoteWebDriver( new URL( remoteUrl ), capabilities );
 
-		driver.get(targetUrl);
-		Thread.sleep(1000);
-		re.check(driver, checkName);
+		driver.get( targetUrl );
+		Thread.sleep( 1000 );
+		re.check( driver, checkName );
 
 		re.capTest();
 	}
@@ -51,13 +56,13 @@ class SauceLabsMobilesTest {
 	@Test
 	public void checkIPhone8Plus() throws Exception {
 		re = new RecheckImpl();
-		re.startTest(testName);
-		DesiredCapabilities capabilities = createCapabilities("iPhone", "iPhone 8 Plus", "11");
-		driver = new RemoteWebDriver(new URL(remoteUrl), capabilities);
+		re.startTest( testName );
+		final DesiredCapabilities capabilities = createCapabilities( "iPhone", "iPhone 8 Plus", "11" );
+		driver = new RemoteWebDriver( new URL( remoteUrl ), capabilities );
 
-		driver.get(targetUrl);
-		Thread.sleep(1000);
-		re.check(driver, checkName);
+		driver.get( targetUrl );
+		Thread.sleep( 1000 );
+		re.check( driver, checkName );
 
 		re.capTest();
 	}
@@ -70,19 +75,20 @@ class SauceLabsMobilesTest {
 
 	@BeforeAll
 	public static void setup() {
-		System.setProperty(RecheckProperties.ROOT_ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY, "0.0");
-		System.setProperty(RecheckProperties.ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY, "0.0");
+		System.setProperty( RecheckProperties.ROOT_ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY, "0.0" );
+		System.setProperty( RecheckProperties.ELEMENT_MATCH_THRESHOLD_PROPERTY_KEY, "0.0" );
 	}
 
-	private static DesiredCapabilities createCapabilities(String browser, String device, String os) {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("username", sauceUserName);
-		capabilities.setCapability("accessKey", sauceAccessKey);
-		capabilities.setCapability("deviceOrientation", "portrait"); // "landscape"
-		capabilities.setCapability("browserName", browser);
-		capabilities.setCapability("device", device);
-		capabilities.setCapability("os_version", os);
-		capabilities.setCapability("name", testName);
+	private static DesiredCapabilities createCapabilities( final String browser, final String device,
+			final String os ) {
+		final DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability( "username", sauceUserName );
+		capabilities.setCapability( "accessKey", sauceAccessKey );
+		capabilities.setCapability( "deviceOrientation", "portrait" ); // "landscape"
+		capabilities.setCapability( "browserName", browser );
+		capabilities.setCapability( "device", device );
+		capabilities.setCapability( "os_version", os );
+		capabilities.setCapability( "name", testName );
 
 		return capabilities;
 	}
