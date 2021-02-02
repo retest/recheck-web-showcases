115add5fee50ff5e1d73f4e750e32d3a79350e52
Martin Vietz <martin.vietz@ubs-hainer.com>
2021-02-02 11:26:59 +0100

    Clean up filter files

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -8,13 +8,16 @@
 // You can find more details and example rules at: 
 // https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
 
-var baseUrl = /http[s]?:\/\/[\w.:\d\-]*/;
 
-function matches(element, diff) {
-	if (diff.expected != null && diff.actual != null) {
-		cleanExpected = diff.expected.replace(baseUrl, '');
-		cleanActual = diff.actual.replace(baseUrl, '');
-		return cleanExpected === cleanActual;
-	}
-	return false;
-}
+
+// Example ignoring the base URL
+//
+//var baseUrl = /http[s]?:\/\/[\w.:\d\-]*/;
+//function matches(element, diff) {
+//	if (diff.expected != null && diff.actual != null) {
+//		cleanExpected = diff.expected.replace(baseUrl, '');
+//		cleanActual = diff.actual.replace(baseUrl, '');
+//		return cleanExpected === cleanActual;
+//	}
+//	return false;
+//}
